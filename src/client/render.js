26e8af6d98a752dd022c4e7b79e8d5f24dda56ac
Borut Vranjes <borut.boa.vranjes@gmail.com>
2020-04-22 13:14:25 +0200

    added project do git
    added project

---
+++ src/client/render.js
@@ -0,0 +1,116 @@
+import { debounce } from 'throttle-debounce';
+import { getAsset } from './assets';
+import { getCurrentState } from './state';
+
+const Constants = require('../shared/constants');
+
+const { PLAYER_RADIUS, PLAYER_MAX_HP, BULLET_RADIUS, MAP_SIZE } = Constants;
+
+const canvas = document.getElementById('game-canvas');
+const context = canvas.getContext('2d');
+setCanvasDimensions();
+
+function setCanvasDimensions() {
+
+  const scaleRatio = Math.max(1, 800 / window.innerWidth);
+  canvas.width = scaleRatio * window.innerWidth;
+  canvas.height = scaleRatio * window.innerHeight;
+}
+
+window.addEventListener('resize', debounce(40, setCanvasDimensions));
+
+function render() {
+  const { me, others, bullets } = getCurrentState();
+  if (!me) {
+    return;
+  }
+
+  // Draw background
+  renderBackground(me.x, me.y);
+
+  // Draw boundaries
+  context.strokeStyle = 'black';
+  context.lineWidth = 20;
+  context.strokeRect(canvas.width / 2 - me.x, canvas.height / 2 - me.y, MAP_SIZE, MAP_SIZE);
+
+  // Draw all bullets
+  bullets.forEach(renderBullet.bind(null, me));
+
+  // Draw all players
+  renderPlayer(me, me);
+  others.forEach(renderPlayer.bind(null, me));
+}
+
+function renderBackground(x, y) {
+  const backgroundX = MAP_SIZE / 2 - x + canvas.width / 2;
+  const backgroundY = MAP_SIZE / 2 - y + canvas.height / 2;
+  const backgroundGradient = context.createRadialGradient(
+    backgroundX,
+    backgroundY,
+    MAP_SIZE / 10,
+    backgroundX,
+    backgroundY,
+    MAP_SIZE / 2,
+  );
+  backgroundGradient.addColorStop(0, 'lightgray');
+  backgroundGradient.addColorStop(1, 'DarkGray');
+  context.fillStyle = backgroundGradient;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function renderPlayer(me, player) {
+  const { x, y, direction } = player;
+  const canvasX = canvas.width / 2 + x - me.x;
+  const canvasY = canvas.height / 2 + y - me.y;
+
+  context.save();
+  context.translate(canvasX, canvasY);
+  context.rotate(direction);
+  context.drawImage(
+    getAsset('shooter.svg'),
+    -PLAYER_RADIUS,
+    -PLAYER_RADIUS,
+    PLAYER_RADIUS * 2,
+    PLAYER_RADIUS * 2,
+  );
+  context.restore();
+
+  context.fillStyle = 'red';
+  context.fillText(
+    player.hp,
+  canvasX - PLAYER_RADIUS+9,
+    canvasY + PLAYER_RADIUS - 15);
+
+}
+
+function renderBullet(me, bullet) {
+  const { x, y } = bullet;
+  context.drawImage(
+    getAsset('bullet.svg'),
+    canvas.width / 2 + x - me.x +10+ BULLET_RADIUS,
+    canvas.height / 2 + y - me.y - BULLET_RADIUS,
+    BULLET_RADIUS * 2,
+    BULLET_RADIUS * 2,
+  );
+}
+
+function renderMainMenu() {
+  const t = Date.now() / 7500;
+  const x = MAP_SIZE / 2 + 800 * Math.cos(t);
+  const y = MAP_SIZE / 2 + 800 * Math.sin(t);
+  renderBackground(x, y);
+}
+
+let renderInterval = setInterval(renderMainMenu, 1000 / 60);
+
+// Replaces main menu rendering with game rendering.
+export function startRendering() {
+  clearInterval(renderInterval);
+  renderInterval = setInterval(render, 1000 / 60);
+}
+
+// Replaces game rendering with main menu rendering.
+export function stopRendering() {
+  clearInterval(renderInterval);
+  renderInterval = setInterval(renderMainMenu, 1000 / 60);
+}
